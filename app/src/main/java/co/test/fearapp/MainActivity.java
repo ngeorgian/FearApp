eedbe25ada3983d3ce5d103ea6a189608b2d1325
Nick <njgeor16@gmail.com>
2017-10-28 16:15:16 -0400

    Basic title Activity interface created. HeartRateSensorActivity created.

--- app/src/main/java/co/test/fearapp/MainActivity.java
+++ app/src/main/java/co/test/fearapp/MainActivity.java
@@ -1,7 +1,9 @@
 package co.test.fearapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    //Changes screen to HeartRateSensorActivity when pressed
+    public void onBeginButtonClick(View view) {
+        Intent i = new Intent(this, HeartRateSensorActivity.class);
+        startActivity(i);
     }
 }
